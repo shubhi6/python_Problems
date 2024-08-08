75d3ab76430dc3bda1c01202837b0b1d2595d0aa
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-08-08 13:03:20 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,6 @@
+# -------------------------PRINT A * PATTERN-----------------------------------------------------
+r=int(input())
+for i in range(r):
+    for j in range(i+1):
+        print("*",end=" ")
+    print("\n")
