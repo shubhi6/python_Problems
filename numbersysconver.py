56bc3ca1b9fff2f58014d80f781f722f7408e9fb
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-08-08 13:03:04 +0530

    Add files via upload

---
+++ numbersysconver.py
@@ -0,0 +1,5 @@
+# -------------------------PROGRAM TO CONVERT DECIMAL NUMBERS TO OTHER NUMBER SYSTEM------------
+dec=int(input("ENTER A NUMBER : "))
+print(bin(dec)," : IN BINARY")
+print(oct(dec)," : IN OCTAL")
+print(hex(dec)," : IN HEXADECIMAL")
