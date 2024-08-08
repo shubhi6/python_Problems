29dd016043fabe16be6bba5985e61186b81cb712
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-08-08 13:01:05 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,4 @@
+# ---------------------------PRINT ADDITION OF TWO NUMBERS-----------------------------------
+n=int(input("ENTER FIRST NUMBER : "))
+m=int(input("ENTER SECOND NUMBER : "))
+print(f"SUM OF {n} AND {m} IS : {n+m}")
