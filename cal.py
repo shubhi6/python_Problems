363e2c57158c724ded5b6e05705a0e3f67399b55
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-08-08 13:01:48 +0530

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,5 @@
+# --------------------------PRINT CALENDER---------------------------------------------------
+import calendar
+yy=int(input("ENTER YEAR : "))
+mm=int(input("ENTER MONTH : "))
+print(calendar.month(yy,mm))
